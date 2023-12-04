2d1310f476dd5954ae99aa473701abd4f7edfaf3
Kaleb Molewe <88307946+KalebMolewe23@users.noreply.github.com>
2023-12-04 17:33:36 +0700

    banner update

---
+++ application/views/home/v_home.php
@@ -0,0 +1,7 @@
+<div class="box1">
+    <center><h3>Jasa Pengembangan Maupun Pembuatan Website & Android</h3><center>
+    <p class="text-white pb-3 animated slideInDown">Perkenalkan, Kami Tim Eresta Developer Menawarkan Kalian Untuk Jasa Dalam Pembuatan, Pengembangan, Dan Custome Website Maupun Android.</p>
+    <p class="text-white pb-3 animated slideInDown">Sesuai Dengan Kebutuhan Anda.</p>
+    <br><br>
+    <a href="#prices"><button class="button_prices"><i class='bx bx-money'></i> Cek Prices</button></a>
+</div>
