1a2d15079c9f638ac621f98026908c45e2160e48
Kaleb Molewe <88307946+KalebMolewe23@users.noreply.github.com>
2023-12-04 18:18:52 +0700

    update our service

--- application/views/home/v_home.php
+++ application/views/home/v_home.php
@@ -5,3 +5,28 @@
     <br><br>
     <a href="#prices"><button class="button_prices"><i class='bx bx-money'></i> Cek Prices</button></a>
 </div>
+
+<section class="home container">
+    <center><h3 class="h3_size">Our Services</h3></center><br>
+    <div class="row_service">
+        <div class="column_service">
+            <div class="card_service">
+                <h4>Full Design & Development</h4>
+                <p>Kami siap membantu kalian dalam membuat tampilan website/android menjadi lebih menarik.</p>
+            </div>
+        </div>
+        <div class="column_service">
+            <div class="card_service">
+                <h4>Full Support</h4>
+                <p>Kami siap membantu untuk pembuatan sistem kalian dari 0 sampai selesai.</p>
+            </div>
+        </div>
+        <div class="column_service">
+            <div class="card_service">
+                <h4>Full Security</h4>
+                <p>Setiap web client kami akan meningkatkan keamanan website agar tidak mudah di HACKER ataupun terkena malware.</p>
+            </div>
+        </div>
+    </div>
+</section> 
+<br>
