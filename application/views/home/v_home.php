1541f505a98b73e82c33bb0d3f28937241fdf7d6
Kaleb Molewe <88307946+KalebMolewe23@users.noreply.github.com>
2023-12-04 19:01:10 +0700

    paket update 1

--- application/views/home/v_home.php
+++ application/views/home/v_home.php
@@ -30,3 +30,76 @@
     </div>
 </section> 
 <br>
+
+<div class="box2">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
+
+<section class="home container">
+    <div class="paket" id="prices">
+        <center><h3 class="h3_size_price">Paket Pembuatan Website</h3></center>
+        <center><p class="p_size_price">Pilih Paket Sesuai Dengan Kebutuhan Sistem Anda</p></center><br><br>
+        <div class="row_paket">
+            <div class="column_paket">
+                <div class="card_paket">
+                    <h4 class="h4_size">Paket Eresta Child</h4>
+                    <p class="p_des">Kategori(Mahasiswa & Company Profile)</p>
+                    <h4 class="h4_price">Range Harga (2 juta - 4 juta)</h4><br>
+                    <h6><i class='bx bxs-crown' ></i> Garansi /Full Support Selamanya</h6>
+                    <h6><i class='bx bx-check' ></i> Bonus Logo Dan 3 Banner</h6>
+                    <h6><i class='bx bx-check' ></i> Free Domain Dan Hosting</h6>
+                    <h6><i class='bx bx-check' ></i> Free Ebook Cara Penggunaan</h6>
+                    <h6><i class='bx bx-check' ></i> Free Revisi Tampilan 2x</h6>
+                    <h6><i class='bx bx-check' ></i> Source Code Full Milik Owner</h6>
+                    <h6><i class='bx bx-x'></i> Pemasangan ke PS/AS</h6>
+                    <!-- <h6><i class='bx bx-check' ></i> Konsultasi Setiap Hari</h6>
+                    <h6><i class='bx bx-x'></i> Free Email Bisnis</h6>-->
+                    <p class="p_price">Penambahan Fitur Diluar Kesepakatan : 100ribu/fitur</p>
+                    <button><i class='bx bx-cart-add'></i> Order Now</button>
+                    <h5 class="des_paket">Perpanjangan 500ribu /tahun</h5>
+                </div>
+            </div>
+            <div class="column_paket">
+                <div class="card_paket">
+                    <h4 class="h4_size">Paket Eresta Adult</h4>
+                    <p class="p_des">Kategori(Toko Online, Top Up Game & Multitasking Lainnya)</p>
+                    <h4 class="h4_price">Range Harga (5 juta - 9 juta)</h4><br>
+                    <h6><i class='bx bxs-crown' ></i> Garansi /Full Support Selamanya</h6>
+                    <h6><i class='bx bx-check' ></i> Bonus Logo Dan Banner</h6>
+                    <h6><i class='bx bx-check' ></i> Free Domain Dan Hosting</h6>
+                    <!-- <h6><i class='bx bx-check' ></i> Konsultasi Setiap Hari</h6>
+                    <h6><i class='bx bx-check' ></i> Free Revisi Tampilan 2x</h6>
+                    <h6><i class='bx bx-check' ></i> Free Ebook Cara Penggunaan</h6>
+                    <h6><i class='bx bx-check' ></i> Maksimal Pengerjaan 1 Bulan</h6> -->
+                    <p class="p_price">Penambahan Fitur Diluar Kesepakatan : 100ribu/fitur</p>
+                    <button><i class='bx bx-cart-add'></i> Order Now</button>
+                    <h5 class="des_paket">Perpanjangan 500ribu /tahun</h5>
+                </div>
+            </div>
+            <div class="column_paket">
+                <div class="card_paket">
+                    <h4 class="h4_size">Paket Eresta Parent</h4>
+                    <p class="p_des">Kategori(PT atau CV)</p>
+                    <h4 class="h4_price">Range Harga (10 juta - 50 juta)</h4><br>
+                    <h6><i class='bx bxs-crown' ></i> Garansi /Full Support Selamanya</h6>
+                    <!-- <h6><i class='bx bx-check' ></i> Bonus Logo Dan Banner</h6>
+                    <h6><i class='bx bx-check' ></i> Free Domain Dan Hosting</h6>
+                    <h6><i class='bx bx-check' ></i> Konsultasi Setiap Hari</h6>
+                    <h6><i class='bx bx-check' ></i> Free Revisi Tampilan 2x</h6>
+                    <h6><i class='bx bx-check' ></i> Free Ebook Cara Penggunaan</h6>
+                    <h6><i class='bx bx-check' ></i> Maksimal Pengerjaan 1 Bulan</h6> -->
+                    <p class="p_price">Penambahan Fitur Diluar Kesepakatan : 100ribu/fitur</p>
+                    <button><i class='bx bx-cart-add'></i> Order Now</button>
+                    <h5 class="des_paket">Perpanjangan 500ribu /tahun</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
